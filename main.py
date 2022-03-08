7b59ca17f33f1de5a608e839974a9a9617863857
Silverwarriorin <46795935+Silverwarriorin@users.noreply.github.com>
2022-03-08 09:56:22 -0500

    Added Setup.py

--- main.py
+++ main.py
@@ -2,105 +2,115 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-# Getting bounds
 
-left_bound = int(input("Left Bound? "))
-right_bound = int(input("Right Bound? "))
+def main():
+# Getting Bounds
 
+    left_bound = int(input("Enter the left bound of the equation: "))
+    right_bound = int(input("Enter the right bound of the equation: "))
 
+# Checking Bounds
+
+    if right_bound < left_bound:
+        print("Left bound MUST be less than right bound!")
+        main()
 # Getting Equation and number of divisions
-equation_string = input("Equation? ")
-partitions = int(input("Partitions? "))
+    equation_string = input("Equation? ")
+    partitions = int(input("Partitions? "))
 
 
 
 
 # Calculate all the points 
 
-f = lambda x : eval(equation_string)
-a = left_bound; b = right_bound; N = partitions
-n = 10 # Use n*N+1 points to plot the function smoothly
+    f = lambda x : eval(equation_string)
+    a = left_bound; b = right_bound; N = partitions
+    n = 10 # Use n*N+1 points to plot the function smoothly
 
-x = np.linspace(a,b,N+1)
-y = f(x)
+    x = np.linspace(a,b,N+1)
+    y = f(x)
 
-X = np.linspace(a,b,n*N+1)
-Y = f(X)
+    X = np.linspace(a,b,n*N+1)
+    Y = f(X)
 
-plt.figure(figsize=(15,5))
-
-
-dx = (b - a)/N
-x_left = np.linspace(a,b-dx,N)
-x_midpoint = np.linspace(dx/2,b - dx/2,N)
-x_right = np.linspace(dx,b,N)
-h = (b - a) / (n - 1)
+    plt.figure(figsize=(15,5))
 
 
-x_mid = (x[:-1] + x[1:])/2
-mid = np.sum(f(x_mid)*dx)
+    dx = (b - a)/N
+    x_left = np.linspace(a,b-dx,N)
+    x_midpoint = np.linspace(dx/2,b - dx/2,N)
+    x_right = np.linspace(dx,b,N)
+    h = (b - a) / (n - 1)
 
 
-
-plt.subplot(1,4,1)
-plt.grid(True, color = 'black', linestyle = '--', linewidth = 1)
-plt.plot(X,Y,'b')
-x_left = x[:-1] # Left endpoints
-y_left = y[:-1]
-plt.plot(x_left,y_left,'b.',markersize=10)
-plt.bar(x_left,y_left,width=(b-a)/N,alpha=0.2,align='edge',edgecolor='b')
-j = np.sum(f(x_left) * dx)
-plt.title('Left Riemann Sum, Ans ≈ {0:.2f}'.format(j))
+    x_mid = (x[:-1] + x[1:])/2
+    mid = np.sum(f(x_mid)*dx)
 
 
 
-
-plt.subplot(1,4,2)
-plt.grid(True, color = 'black', linestyle = '--', linewidth = 1)
-plt.plot(X,Y,'b')
-x_mid = (x[:-1] + x[1:])/2 # Midpoints
-y_mid = f(x_mid)
-plt.plot(x_mid,y_mid,'b.',markersize=10)
-plt.bar(x_mid,y_mid,width=(b-a)/N,alpha=0.2,edgecolor='b')
-plt.title('Midpoint Riemann Sum, Ans ≈ {0:.2f}'.format(mid))
+    plt.subplot(1,4,1)
+    plt.grid(True, color = 'black', linestyle = '--', linewidth = 1)
+    plt.plot(X,Y,'b')
+    x_left = x[:-1] # Left endpoints
+    y_left = y[:-1]
+    plt.plot(x_left,y_left,'b.',markersize=10)
+    plt.bar(x_left,y_left,width=(b-a)/N,alpha=0.2,align='edge',edgecolor='b')
+    j = np.sum(f(x_left) * dx)
+    plt.title('Left Riemann Sum, Ans ≈ {0:.2f}'.format(j))
 
 
 
 
-plt.subplot(1,4,3)
-plt.grid(True, color = 'black', linestyle = '--', linewidth = 1)
-plt.plot(X,Y,'b')
-x_right = x[1:] # Left endpoints
-y_right = y[1:]
-plt.plot(x_right,y_right,'b.',markersize=10)
-plt.bar(x_right,y_right,width=-(b-a)/N,alpha=0.2,align='edge',edgecolor='b')
-right_riemann_sum = np.sum(f(x_right) * dx)
-plt.title('Right Riemann Sum, Ans ≈ {0:.2f}'.format(right_riemann_sum))
+    plt.subplot(1,4,2)
+    plt.grid(True, color = 'black', linestyle = '--', linewidth = 1)
+    plt.plot(X,Y,'b')
+    x_mid = (x[:-1] + x[1:])/2 # Midpoints
+    y_mid = f(x_mid)
+    plt.plot(x_mid,y_mid,'b.',markersize=10)
+    plt.bar(x_mid,y_mid,width=(b-a)/N,alpha=0.2,edgecolor='b')
+    plt.title('Midpoint Riemann Sum, Ans ≈ {0:.2f}'.format(mid))
 
 
 
 
-plt.subplot(1,4,4)
-plt.grid(True, color = 'black', linestyle = '--', linewidth = 1)
-plt.plot(X,Y,'b')
-
-plt.plot(x_right,y_right,'b.',markersize=10)
-for i in range(N):
-    xs = [x[i],x[i],x[i+1],x[i+1]]
-    ys = [0,f(x[i]),f(x[i+1]),0]
-    plt.fill(xs,ys,'b',edgecolor='b',alpha=0.2)
-
-h = float(b - a) / n
-s = 0.0
-s += f(a)/2.0
-for i in range(1, n):
-    s += f(a + i*h)
-s += f(b)/2.0
-ans =  s * h
-plt.title('Trapezoid Rule, N ≈ {0:.2f}'.format(ans))
-
-plt.show()
+    plt.subplot(1,4,3)
+    plt.grid(True, color = 'black', linestyle = '--', linewidth = 1)
+    plt.plot(X,Y,'b')
+    x_right = x[1:] # Left endpoints
+    y_right = y[1:]
+    plt.plot(x_right,y_right,'b.',markersize=10)
+    plt.bar(x_right,y_right,width=-(b-a)/N,alpha=0.2,align='edge',edgecolor='b')
+    right_riemann_sum = np.sum(f(x_right) * dx)
+    plt.title('Right Riemann Sum, Ans ≈ {0:.2f}'.format(right_riemann_sum))
 
 
 
 
+    plt.subplot(1,4,4)
+    plt.grid(True, color = 'black', linestyle = '--', linewidth = 1)
+    plt.plot(X,Y,'b')
+
+    plt.plot(x_right,y_right,'b.',markersize=10)
+    for i in range(N):
+        xs = [x[i],x[i],x[i+1],x[i+1]]
+        ys = [0,f(x[i]),f(x[i+1]),0]
+        plt.fill(xs,ys,'b',edgecolor='b',alpha=0.2)
+
+    h = float(b - a) / n
+    s = 0.0
+    s += f(a)/2.0
+    for i in range(1, n):
+        s += f(a + i*h)
+    s += f(b)/2.0
+    ans =  s * h
+    plt.title('Trapezoid Rule, N ≈ {0:.2f}'.format(ans))
+
+    plt.show()
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,57 @@
+from distutils.core import setup
+from setuptools import find_packages
+import os
+
+# Optional project description in README.md:
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
+setup(
+
+# Project name: 
+name='Math Integral Finder',
+
+# Packages to include in the distribution: 
+packages=find_packages(','),
+
+# Project version number:
+version='2.5',
+
+# List a license for the project, eg. MIT License
+license='MIT',
+
+# Short description of your library: 
+description='Finds integrals of an equation',
+
+# Long description of your library: 
+long_description=long_description,
+long_description_content_type='text/markdown',
+
+# Your name: 
+author='Gabriel Womelsdorf',
+
+# Your email address:
+author_email='837297@lcps.org',
+
+# Link to your github repository or website: 
+url='https://github.com/Silverwarriorin/MathIntegralFinder',
+
+# Download Link from where the project can be downloaded from:
+download_url='',
+
+# List of keywords: 
+keywords=[],
+
+# List project dependencies: 
+install_requires=[
+    '<matplotlib>',
+    '<numpy>'
+],
+
+# https://pypi.org/classifiers/ 
+classifiers=[]
+)
